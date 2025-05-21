94b084bc1978b1201039b191b511274146e02b3b
stefanozugni <156177552+stefanozugni@users.noreply.github.com>
2025-05-21 10:11:49 +0200

    Update nomination.model.ts

--- src/models/nomination.model.ts
+++ src/models/nomination.model.ts
@@ -1,14 +1,10 @@
- Ceremony: number;
-  Year: string;
-  Class: string;
-  CanonicalCategory: string;
-  Category: string;
-  Film: string;
-  FilmId?: string;
-  Name?: string;
-  Nominees?: string!; 
-  NomineeIds?: string;
-  Winner?: string; 
-  Detail?: string;
-  Note?: string;
-  Citation?: string;
+category: string;
+    year: string;
+    nominees: string[];
+    movies: {
+        title: string;
+        tmdb_id: number;
+        imdb_id: string;
+    }[];
+    won: boolean;
+}
